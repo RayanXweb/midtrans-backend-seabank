62e730a071c0b33c220402901f8cb504467a646e
RayanXweb <rohmanjakarta15@gmail.com>
2026-02-18 06:49:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+require("dotenv").config()
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+app.get("/", (req,res)=>res.send("Backend Running"))
+
+app.post("/api/topup",(req,res)=>{
+  res.json({token:"MIDTRANS_SNAP_TOKEN_SAMPLE"})
+})
+
+app.listen(process.env.PORT || 3000)
